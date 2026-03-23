64aef1557c322a186a54a25d69d52567428e566e
julie-12rpl <ksj885709@gmail.com>
2026-03-23 10:00:31 +0700

    tahap ulang persiapan ukk

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\CashierController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Product;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
